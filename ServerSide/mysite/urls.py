e15168f70aa35983e6bc4f82208ff34bcaf1033b
yzf <375007501@qq.com>
2014-05-06 21:51:43 +0800

    服务端代码

---
+++ ServerSide/mysite/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from mysite.wetalk.views import *
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+    (r'^register/$', register),
+    (r'^login/$', login),
+)
